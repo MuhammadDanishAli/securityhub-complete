ffba2ba7718256b4b67fa4d331b2dac9572d9531
Muhammad Danish <muhammad786dk47dk@gmail.com>
2025-05-01 07:27:30 +0500

    Updated api/views.py and api/urls.py for login

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,17 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from . import views
 
-router = DefaultRouter()
-router.register(r'sensors', views.SensorViewSet)
-router.register(r'sensor-data', views.SensorDataViewSet)
-router.register(r'alertlogs', views.AlertlogViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('test-auth/', views.TestAuthView.as_view(), name='test_auth'),
-    path('mode/', views.ModeView.as_view(), name='mode'),
-    path('sensor-control/', views.SensorControlView.as_view(), name='sensor_control'),
-    path('sensor-status/', views.SensorStatusView.as_view(), name='sensor_status'),
 ]
