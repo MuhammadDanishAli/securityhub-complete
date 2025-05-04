b64d3911fb91b69a24ce83cc436827e4017e520c
Muhammad Danish <muhammad786dk47dk@gmail.com>
2025-05-04 12:01:45 +0500

    Clean backend files from frontend repo

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import LoginView, PublishView, SensorStatusView, ModeView
-
-urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('publish/', PublishView.as_view(), name='publish'),
-    path('sensor-status/', SensorStatusView.as_view(), name='sensor-status'),
-    path('mode/', ModeView.as_view(), name='mode'),
-]
--- backend/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Sensor, SensorData, Alertlog, SystemMode
-
-admin.site.register(Sensor)
-admin.site.register(SensorData)
-admin.site.register(Alertlog)
-admin.site.register(SystemMode)
